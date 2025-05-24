05e15a44a5d66ec0a82f75658e6cd51391d9ec9e
Emil Jandal <emil.jandal@gmail.com>
2025-05-24 08:59:45 +0200

    Simple UI and TaskItem class added.

---
+++ SimpleToDo/Models/TaskItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleToDo.Models
+{
+    public class TaskItem
+    {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public override string ToString()
+        {
+            return $"{(IsCompleted ? "[X]" : "[ ]")} {Title} - {DueDate:dd.MM.yyyy}";
+        }
+    }
+}
